c42758a3363cb4d3ca2e4e25d68112e6ef9aca56
Sandy Ramirez <sandyramirezp@gmail.com>
2012-04-26 14:31:14 -0400

    Proyecto general
    Esta versión del proyecto no tiene modelos, controladores o vistas
    creadas.

---
+++ InicialProject/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InicialProject::Application.config.secret_token = '75f479088e0d984c98e6917b8d8e3b0015016ede184b3d3047778a52b511945a741b64b22a09f06d4390f52fce0001ede71637a6e2feda9f572481b666ec4f18'
